66da89f3c2bddf0ec4ce27cc6b3a5d451d88aa43
Catherine78d <cath.doucet@gmail.com>
2020-11-30 17:24:38 +0100

    dev entité + catégorie + comment + inscription

--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -3,7 +3,10 @@
 namespace App\DataFixtures;
 
 
+use Faker\Factory;
 use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -11,19 +14,71 @@ class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++)
-        {
-            $article = new Article;
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("https://picsum.photos/200/300")
-                    ->setCreatedAt(new \DateTime());
+        // Création de 3 catégories
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category;
 
-            $manager->persist($article);
+            $category->settitle($faker->sentence())
+                    ->setDescription($faker->paragraph()); 
 
+            $manager->persist($category);
+
+            // Création de 4 à 6 articles
+            for($j = 1; $j <= mt_rand(4,6); $j++)
+            {
+                $article = new Article;
+
+                $content = '<p>' . join($faker->paragraphs(5), '<p><p>') . '</p>';
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+                
+                $manager->persist($article);
+
+                // Création de 4 à 10 commentaires
+                for($k = 1; $k <= mt_rand(4,10); $k++)
+                {
+                    
+                    $comment = new Comment;
+
+                    $content = '<p>' . join($faker->paragraphs(2), '<p><p>') . '</p>';
+
+                    $now = new \DateTime;
+                    $interval = $now->diff($article->getCreatedAt()); // retourne le temps (timestamp) entre la date de création des articles et aujourd'hui
+                    $days = $interval->days;  // nombre de jour entre la date de création des articles et maintenant 
+                    $minimum = '-' . $days . ' days'; /* -100 days le but est d'avoir des dates de commentaires, entre par exemple -100 days et aujourd'hui */
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween($minimum))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
         }
+
+        $manager->flush();      
+
+        // for($i = 1; $i <= 10; $i++)
+        // {
+        //     $article = new Article;
+
+        //     $article->setTitle("Titre de l'article n°$i")
+        //             ->setContent("<p>Contenu de l'article n°$i</p>")
+        //             ->setImage("https://picsum.photos/200/300")
+        //             ->setCreatedAt(new \DateTime());
+
+        //     $manager->persist($article);
+
+        // }
         
-        $manager->flush();
+        // $manager->flush();
+
     }
 }
